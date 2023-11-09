65f9366924dffa30a145ecf7741e7796f583f291
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-09 21:34:15 +0200

    Add files via upload

---
+++ sum and defference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    int sum;
+    int difference;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    sum=a+b;
+    cout<<sum<<endl;
+    difference=a-b;
+    cout<<difference;
+    return 0 ;
+}
