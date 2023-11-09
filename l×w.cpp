ca2be7473e12281bb8cda7fd5cb888d21e2efd07
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-09 21:07:39 +0200

    Add files via upload

---
+++ l×w.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A ;
+    int l ;
+    int w ;
+    cout <<"l= ";
+    cin>>l;
+    cout <<"w= ";
+    cin>>w;
+    A=l*w;
+    cout <<A;  
+    return 0;
+}
