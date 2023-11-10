610e60de4580b7ed8d42eaf6450826a0cd3c608f
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-10 14:10:32 +0200

    Add files via upload

---
+++ div.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int C;
+    int D;
+    cout << "n=";
+    cin >> n;
+    C = 5;
+    cout << "C=" << C << endl;
+    D = n / C;
+    cout << "D=" << D;
+    return 0;
+}
