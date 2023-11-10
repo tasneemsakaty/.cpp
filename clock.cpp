61f28648bb910bd915dfa146a089137dcdcaa7af
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-10 15:00:07 +0200

    Add files via upload

---
+++ clock.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int h;
+    int m;
+    int s;
+    int ms;
+    int a;
+    int b;
+    int c;
+    h= a*ms;
+    a=3600000;
+    cout <<"h=";
+    cin>>h;
+    m=b*ms;
+    b=60000;
+    cout <<"m=";
+    cin>>m;
+    s=c*ms;
+    c=1000;
+    cout <<"s=";
+    cin>>s;
+
+    cout<<"ms="<<ms;
+    return 0;
+}
