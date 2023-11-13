5b69e3e0cc5943a2befa8a7ecbba6e24372f19e5
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-13 14:40:20 +0200

    Add files via upload

---
+++ b assign a.cpp
@@ -0,0 +1,15 @@
+//TO prove a=b is different than b=a
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    a=5;
+    b=7;
+    cout <<"original value of a = "<< a << endl;
+    cout <<"original value of b = "<< b << endl;
+    b=a;
+    cout <<"last value of a = "<< a << endl;
+    cout <<"last value of b = "<< b << endl;
+    return 0;
+}
