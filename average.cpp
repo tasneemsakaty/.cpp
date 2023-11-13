5bdc5c5dfa261d6b54a6d9ff558cd64006a80096
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-13 17:28:41 +0200

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float First_number;
+    float Second_number;
+    float Average;
+    cout <<"First_number = ";
+    cin>>First_number;
+    cout <<"Second_number = ";
+    cin>>Second_number;
+    Average = (First_number+Second_number)/2;
+    cout <<"Average of the two entered numberes = "<<Average;
+    return 0;
+}
