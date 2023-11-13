25f7687e69cb1c6801a99958de2718c384f3cb23
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-13 15:40:56 +0200

    Add files via upload

---
+++ ASCII code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    char character;
+    cout <<"character = ";
+    cin>>character;
+    number = character;
+    cout <<"the character "<<character;
+    cout <<" is represented as the number "<<number;
+    cout <<" in the computer";
+    return 0;
+}
