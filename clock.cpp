a493ff3bf0d1057576ad1b81ba0f3eeb701b05ae
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-11-20 18:45:38 +0200

    Clock.cpp

--- clock.cpp
+++ clock.cpp
@@ -5,22 +5,13 @@ int main() {
     int m;
     int s;
     int ms;
-    int a;
-    int b;
-    int c;
-    h= a*ms;
-    a=3600000;
     cout <<"h=";
     cin>>h;
-    m=b*ms;
-    b=60000;
     cout <<"m=";
     cin>>m;
-    s=c*ms;
-    c=1000;
     cout <<"s=";
     cin>>s;
-
+    ms=(3600000*h)+(60000*m)+(1000*ms);
     cout<<"ms="<<ms;
     return 0;
 }
