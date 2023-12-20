087fa509a17a90ccb3fc936a24d0930a214c7ed2
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-12-20 17:47:34 +0200

    Add files via upload

--- calculating processes.cpp
+++ calculating processes.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 using namespace std;
 int main() {
-    float a;
-    float b;
-    float Addition;
-    float Subtraction;
-    float Multiplication;
-    float Division;
+    float a,b,Result;
+    char Symbol;
     cout << "a = ";
     cin >> a;
     cout << "b = ";
     cin >> b;
-    Addition = a + b;
-    cout << "Addition = " << Addition << endl;
-    Subtraction = a - b;
-    cout << "Subtraction = " << Subtraction << endl;
-    Multiplication = a * b;
-    cout << "Multiplication = " << Multiplication << endl;
-    Division = a / b;
-    cout << "Division = " << Division;
+    cout << "Symbol of the process is ";
+    cin >> Symbol;
+    switch (Symbol){
+    case'+' :
+        Result=a+b;
+        cout<<""<<a<<"+"<<b<<"="<<Result<<" \n"; break;
+    case'-' :
+        Result=a-b;
+        cout<<""<<a<<"-"<<b<<"="<<Result<<" \n"; break;
+    case'*' :
+        Result=a*b;
+        cout<<""<<a<<"*"<<b<<"="<<Result<<" \n"; break;   
+    case'/' :
+        if (b!=0){
+        Result=a/b;
+        cout<<""<<a<<"/"<<b<<"="<<Result<<" \n"; break;
+        }
+        else 
+        cout <<"Division by zero is undefined.\n"; break;
+    default :
+        cout << "Invalid operator.\n";
+    }
     return 0;
 }
