62c301c680bc0c0baf27f70fca685201277a5f6f
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-12-20 18:33:35 +0200

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Plz enter a number : ";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<""<<num<<" is even.\n";
+    }
+    else
+    {
+    cout<<""<<num<<" is odd.\n";
+    }
+    return 0;
+}
