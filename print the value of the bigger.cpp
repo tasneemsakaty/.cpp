ee26ecd1cddb0174b577dfc7b0cada6a384751b8
tasneemsakaty <150383963+tasneemsakaty@users.noreply.github.com>
2023-12-20 13:58:30 +0200

    Add files via upload

---
+++ print the value of the bigger.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    //a is the first num; b is the second num
+    cout <<"The first num is ";
+    cin>>a;
+    cout <<"The second num is ";
+    cin>>b;
+    if (a>b){
+        cout<<"The bigger num is "<<a<<" ";
+    }
+    else 
+    cout<<"The bigger num is "<<b<<" ";
+    return 0;
+}
